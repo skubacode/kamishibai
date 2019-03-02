5a0920a122123b97758cad36e3095351b653ba5b
Xavier Albaiges <xalbaiges@yahoo.es>
2019-03-02 01:35:15 +0100

    added bare bone version for jrpg

---
+++ jrpg/main.c
@@ -0,0 +1,236 @@
+#include<stdio.h>
+
+#define _INDEX_MAIN_MENU_           0
+#define _INDEX_LOCAL_MAP_            1
+#define _INDEX_WORLD_MAP_          2
+#define _INDEX_BATTLE_                     3
+#define _INDEX_IN_GAME_MENU_   4
+
+#define _GAME_MODES_NUMBER_  5
+
+struct  Game_Mode
+{
+  int id;
+  void (*fn_update)(float elapsed_time) ;
+  void (*fn_render)();
+  void (*fn_on_enter)();
+  void (*fn_on_exit)();
+};
+
+/**************************************************
+ * GAME MODE: 
+ * MAIN MENU 
+ **************************************************/
+void main_menu_update( float elapsed_time ) 
+{
+  puts("main menu update");
+}//main_menu_update
+
+void main_menu_render()
+{
+  puts("main menu render");	
+}//main_menu_render
+
+void main_menu_enter()
+{
+  puts("main menu enter");	
+}//main_menu_enter
+
+void main_menu_exit()
+{
+  puts("main menu exit");	
+}//main_menu_exit
+
+/**************************************************
+ * GAME MODE: 
+ * LOCAL MAP
+ **************************************************/
+void local_map_update( float elapsed_time ) 
+{
+  puts("local map update");
+}//local_map_update
+
+void local_map_render()
+{
+  puts("local map render");	
+}//local_map_render
+
+void local_map_enter()
+{
+  puts("local map enter");	
+}//local_map_enter
+
+void local_map_exit()
+{
+  puts("local map exit");	
+}//local_map_exit
+
+
+/**************************************************
+ * GAME MODE: 
+ * WORLD MAP
+ **************************************************/
+void world_map_update( float elapsed_time ) 
+{
+  puts("world map update");
+}//world_map_update
+
+void world_map_render()
+{
+  puts("world map render");	
+}//world_map_render
+
+void world_map_enter()
+{
+  puts("world map enter");	
+}//world_map_enter
+
+void world_map_exit()
+{
+  puts("world map exit");	
+}//world_map_exit
+
+/**************************************************
+ * GAME MODE: 
+ * BATTLE
+ **************************************************/
+void battle_update( float elapsed_time ) 
+{
+  puts("battle update");
+}//battle_update
+
+void battle_render()
+{
+  puts("battle render");	
+}//battle_render
+
+void battle_enter()
+{
+  puts("battle enter");	
+}//battle_enter
+
+void battle_exit()
+{
+  puts("battle exit");	
+}//battle_exit
+
+/**************************************************
+ * GAME MODE: 
+ * IN GAME MENU
+**************************************************/
+void in_game_menu_update( float elapsed_time ) 
+{
+  puts("in game menu update");
+}//battle_update
+
+void in_game_menu_render()
+{
+  puts("in game menu render");	
+}//battle_render
+
+void in_game_menu_enter()
+{
+  puts("in game menu enter");	
+}//void in_game_menu_enter()
+
+void in_game_menu_exit()
+{
+  puts("in game menu exit");	
+}//in_game_menu_exit
+
+
+struct State_Machine
+{
+  struct Game_Mode game_modes[_GAME_MODES_NUMBER_];
+  int index_current_game_mode;
+
+  void (*fn_update)(float elapsed_time) ;
+  void (*fn_render)();
+  void (*fn_change)();
+  void (*fn_add)();  
+};//State_Machine
+
+void state_machine_update(float elapsed_time)
+{
+  puts("state machine update");	
+}//state_machine_update
+
+void state_machine_render( const struct Game_Mode *game_mode )
+{
+  puts("state machine render");
+  game_mode->fn_render();	  
+}//state_machine_render
+
+void state_machine_change()
+{
+  puts("state machine change");
+}//state_machine_change
+
+void state_machine_add()
+{
+  puts("state machine add");
+}//state_machine_add
+
+
+int main(int argc, char** argv) 
+{
+  puts("jrpg");	
+  /* Game Modes initialization */
+  struct  Game_Mode menu_game_mode;
+  menu_game_mode.id = _INDEX_MAIN_MENU_;
+  menu_game_mode.fn_update = main_menu_update;
+  menu_game_mode.fn_render = main_menu_render;
+  menu_game_mode.fn_on_enter = main_menu_enter;
+  menu_game_mode.fn_on_exit = main_menu_exit;
+
+  struct  Game_Mode local_map_game_mode;
+  local_map_game_mode.id = _INDEX_LOCAL_MAP_;
+  local_map_game_mode.fn_update = local_map_update;
+  local_map_game_mode.fn_render =  local_map_render;
+  local_map_game_mode.fn_on_enter =   local_map_enter;
+  local_map_game_mode.fn_on_exit =  local_map_exit;
+  
+  struct  Game_Mode world_map_game_mode;
+  world_map_game_mode.id = _INDEX_WORLD_MAP_;
+  world_map_game_mode.fn_update =  world_map_update;
+  world_map_game_mode.fn_render =  world_map_render;
+  world_map_game_mode.fn_on_enter =  world_map_enter;
+  world_map_game_mode.fn_on_exit =  world_map_exit;
+
+  struct  Game_Mode battle_game_mode;
+  battle_game_mode.id = _INDEX_BATTLE_;
+  battle_game_mode.fn_update = world_map_update;
+  battle_game_mode.fn_render = world_map_render;
+  battle_game_mode.fn_on_enter = world_map_enter;
+  battle_game_mode.fn_on_exit = world_map_exit;
+
+  struct Game_Mode in_game_menu_game_mode;
+  in_game_menu_game_mode.id = _INDEX_IN_GAME_MENU_;
+  in_game_menu_game_mode.fn_update = in_game_menu_update;
+  in_game_menu_game_mode.fn_render = in_game_menu_render;
+  in_game_menu_game_mode.fn_on_enter = in_game_menu_enter;
+  in_game_menu_game_mode.fn_on_exit = in_game_menu_exit;
+  /* Eo Game Mode initialization */
+
+  /* Statem Machine initialization*/
+ struct State_Machine state_machine;
+ state_machine.game_modes[_INDEX_MAIN_MENU_] = menu_game_mode;
+ state_machine.game_modes[_INDEX_LOCAL_MAP_] = local_map_game_mode;
+ state_machine.game_modes[_INDEX_WORLD_MAP_] = world_map_game_mode;
+ state_machine.game_modes[_INDEX_BATTLE_] = battle_game_mode;
+ state_machine.game_modes[_INDEX_IN_GAME_MENU_] = in_game_menu_game_mode;
+ state_machine.index_current_game_mode = _INDEX_MAIN_MENU_;  //menu_game_mode
+
+ state_machine.fn_update = state_machine_update;
+ state_machine.fn_render = state_machine_render;
+ state_machine.fn_change = state_machine_change;
+ state_machine.fn_add = state_machine_add;
+
+ state_machine.fn_render( &state_machine.game_modes[state_machine.index_current_game_mode] );
+
+ // Command
+  state_machine.index_current_game_mode = _INDEX_WORLD_MAP_;  //menu_game_mode
+  state_machine.fn_render( &state_machine.game_modes[state_machine.index_current_game_mode] );
+
+  return 0;
+}//main
---
+++ kamishibai.c
@@ -0,0 +1,192 @@
+#include<windows.h>
+#include<stdio.h>
+#include <windowsx.h>
+#include <d3d9.h>
+
+// include the Direct3D Library file
+#pragma comment (lib, "d3d9.lib")
+
+// url ref: http://www.winprog.org/tutorial/simple_window.html
+
+
+// global declarations
+LPDIRECT3D9 d3d;    // the pointer to our Direct3D interface
+LPDIRECT3DDEVICE9 d3ddev;    // the pointer to the device class
+
+// function prototypes
+void initD3D(HWND hWnd);    // sets up and initializes Direct3D
+void render_frame(void);    // renders a single frame
+void cleanD3D(void);    // closes Direct3D and releases memory
+
+const char g_szClassName[]="myWinowClass";
+
+// Step 4 : The Window Procedure
+/*
+If the message loop is the heart of the program, the window procedure is the brain. 
+This is where all the messages that are sent to our window get processed.
+*/
+/*
+The window procedure is called for each message, the HWND parameter is the handle of your window, the one that the message applies to. 
+This is important since you might have two or more windows of the same class and they will use the same window procedure (WndProc()). 
+The difference is that the parameter hwnd will be different depending on which window it is. 
+For example when we get the WM_CLOSE message we destroy the window. Since we use the window handle that we received as the first paramter, 
+any other windows will not be affected, only the one that the message was intended for.
+*/
+
+/*
+WM_CLOSE is sent when the user presses the Close Button  [x] or types Alt-F4. 
+This will cause the window to be destroyed by default, but I like to handle it explicitly, since this is the perfect spot to do cleanup checks, 
+or ask the user to save files etc. before exiting the program.
+
+When we call DestroyWindow() the system sends the WM_DESTROY message to the window getting destroyed, in this case it's our window, 
+and then destroys any remaining child windows before finally removing our window from the system. Since this is the only window in our program, 
+we are all done and we want the program to exit, so we call PostQuitMessage(). This posts the WM_QUIT message to the message loop. 
+We never receive this message, because it causes GetMessage() to return FALSE, and as you'll see in our message loop code, 
+when that happens we stop processing messages and return the final result code, the wParam of WM_QUIT which happens to be the value we passed into 
+PostQuitMessage(). The return value is only really useful if your program is designed to be called by another program and you want to return a specific value.
+*/
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+  switch(msg)
+  {
+   case WM_CLOSE:
+        DestroyWindow(hwnd);
+		break;
+   case WM_DESTROY:
+        PostQuitMessage(0);
+		break;
+   default:
+		return DefWindowProc(hwnd, msg, wParam, lParam);
+  }//eo switch	  
+  return 0;
+}// Eo WndProc
+
+// eo STEP 4.
+
+
+
+
+/*
+WINAPI is a macro that evaluates to __stdcall, a Microsoft-specific keyword that specifies a calling 
+convention where the callee cleans the stack. The function's caller and callee need to agree on a calling 
+convention to avoid corrupting the stack.
+
+It's Windows-specific. It specifies the calling convention. 
+WinMain gets called by Windows, and this ensures that the caller and callee agree on the calling convention.
+*/
+int WINAPI WinMain  /* Entry point for win32 apps */
+( 
+ HINSTANCE hInstance, /*handle pointer to the module (module is code loaded in memory .exe or .dll, here is used only one module 
+                        and is used to load resources or somelike stuff */
+ HINSTANCE hPrevInstance, /*always NULL form win32 programs win32 ignores it. It was the previous instance of the program in other execution*/
+ LPSTR lpCmdLine, /* Command line arguments programe name not included */
+ int nCmdShow /*integer number needed by ShowWindow() function */
+)
+{
+ // WINAPI calling convention _stdcall
+ // Win32 use some data types definitions
+ // LPSTR char *
+ // LPCSTR constant long pointer to string  
+ // UINT unsigned int
+ // ... 
+ WNDCLASSEX wc;
+ HWND hwnd;
+ MSG msg;
+ 
+ 
+ // Step 1: Register the Window Class
+ // Store information about a type of window. This way you can register a class once
+ // and create as many windows as you want from it without having specify those attribs over and over.
+ wc.cbSize        = sizeof(WNDCLASSEX); // The size of the wc structure
+ wc.style         = 0; // Class Styles (CS_*), not to be confused with Window Styles (WS_*) This can usually be set to 0.
+ wc.lpfnWndProc   = WndProc; // Pointer to the window procedure for this window class.
+ wc.cbClsExtra    = 0; // Amount of extra data allocated for this class in memory. Usually 0.
+ wc.cbWndExtra    = 0; // Amount of extra data allocated in memory per window of this type. Usually 0.
+ wc.hInstance     = hInstance; // Handle to application instance (that we got in the first parameter of WinMain()).
+ wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION); // Large (usually 32x32) icon shown when the user presses Alt+Tab.
+ wc.hCursor       = LoadCursor(NULL, IDC_ARROW); // Cursor that will be displayed over our window.
+ wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1); // Background Brush to set the color of our window.
+ wc.lpszMenuName  = NULL; // Name of a menu resource to use for the windows with this class.
+ wc.lpszClassName = g_szClassName; // Name to identify the class with.
+ wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION); // Small (usually 16x16) icon to show in the taskbar and in the top left corner of the window.
+ 
+ if(!RegisterClassEx(&wc))
+ {
+  MessageBox(NULL, "Window Registration Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK );
+  return 0;  
+ }// Eoif
+ 
+ // Eo Step 1.
+ 
+ // Step 2: Creating the window
+ hwnd = CreateWindowEx(
+                       WS_EX_CLIENTEDGE, // the extended windows style
+                       g_szClassName, // tells the system what kind of window to create
+                       "The Title of my Window",
+                       WS_OVERLAPPEDWINDOW, // Window Style
+                       CW_USEDEFAULT, // Window position TOP LEFT X 
+					   CW_USEDEFAULT, // Window position TOP LEFT Y 
+					   240, 120,      // Window Size with, Window Size height
+					   NULL,          // Parent Window handle
+					   NULL,          // the menu handle 
+					   hInstance,     // application instance handle
+					   NULL			  // pointer to window creation data.Send additional data to the window that is being created( almost never use ) 
+                      );
+					  
+ if(hwnd == NULL)
+ {
+  MessageBox(NULL, "Window Creation Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK); 	 
+  return 0; 
+ }//eoif	
+ 
+ /*
+ The nCmdShow parameter is optional, you could simply pass in SW_SHOWNORMAL all the time and be done with it. However using the 
+ parameter passed into WinMain() gives whoever is running your program to specify whether or not they want your window to start off visible, 
+ maximized, minimized, etc... You will find options for these in the properties of windows shortcuts, and this parameter is how the choice is carried out.
+ */
+ ShowWindow(hwnd, nCmdShow);
+ UpdateWindow(hwnd);
+ // Eo Step2.
+
+ // Step 3: The Messge Loop
+ // This is the heart of the whole program, pretty much everything that your program does passes through this point of control.
+ while(GetMessage(&msg, NULL, 0, 0)> 0 ) //
+ {
+   /*
+   GetMessage() gets a message from your application's message queue. Any time the user moves the mouse, types on the keyboard, 
+   clicks on your window's menu, or does any number of other things, messages are generated by the system and entered into your program's message queue. 
+   By calling GetMessage() you are requesting the next available message to be removed from the queue and returned to you for processing. 
+   If there is no message, GetMessage() Blocks.
+   */	 
+   TranslateMessage(&msg); // does some additional processing on keyboard events like generating WM_CHAR messages to go along with WM_KEYDOWN messages.
+   DispatchMessage(&msg); /*  sends the message out to the window that the message was sent to. This could be our main window or it could be another one, 
+                              or a control, and in some cases a window that was created behind the scenes by the sytem or another program. 
+							  This isn't something you need to worry about because all we are concerned with is that we get the message and send it out, 
+                              the system takes care of the rest making sure it gets to the proper window  */
+ }//eo while
+ // Eo Step 3.
+ 
+ return msg.wParam;
+}// Eo WinMain
+
+// this function initializes and prepares Direct3D for use
+void initD3D(HWND hWnd)
+{
+    d3d = Direct3DCreate9(D3D_SDK_VERSION);    // create the Direct3D interface
+
+    D3DPRESENT_PARAMETERS d3dpp;    // create a struct to hold various device information
+
+    ZeroMemory(&d3dpp, sizeof(d3dpp));    // clear out the struct for use
+    d3dpp.Windowed = TRUE;    // program windowed, not fullscreen
+    d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;    // discard old frames
+    d3dpp.hDeviceWindow = hWnd;    // set the window to be used by Direct3D
+
+    // create a device class using this information and information from the d3dpp stuct
+    d3d->CreateDevice(D3DADAPTER_DEFAULT,
+                      D3DDEVTYPE_HAL,
+                      hWnd,
+                      D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+                      &d3dpp,
+                      &d3ddev);
+}
